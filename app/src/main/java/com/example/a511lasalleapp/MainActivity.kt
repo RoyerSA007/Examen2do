b9c99eeea5dd277403c9518d1b94f00b3ebd5605
Juan Frausto <juanfr97@hotmail.com>
2024-09-24 18:20:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/a511lasalleapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.a511lasalleapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a511lasalleapp.ui.theme._511LaSalleAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _511LaSalleAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
